ddefeb93e0ee01008202b70fac642c9f6e5e5145
Rassilion <denizgezgin365@gmail.com>
2016-01-10 14:16:26 +0200

    slugify names

--- models.py
+++ models.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 from config import db
+import re
+
+
+def slugify(value, separator="-"):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    import unicodedata
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip())
+    return unicode(re.sub('[\s]+', separator, value))
 
 
 class Manga(db.Model):
@@ -10,7 +22,7 @@ class Manga(db.Model):
     chapters = db.relationship('Chapter', backref='manga', lazy='dynamic')
 
     def __init__(self, name=None, url=None):
-        self.name = name
+        self.name = unicode(slugify(name, separator=' '))
         self.url = url
 
     def __unicode__(self):
@@ -26,10 +38,8 @@ class Chapter(db.Model):
     cdn_id = db.Column(db.Integer)
     downloaded = db.Column(db.Boolean, default=False)
 
-    def __init__(self, name, url="http://"):
-        a = name.replace("/", ",")
-        a = a.replace("\\", ",").replace(u"\u2018", "'").replace(u"\u2019", "'")
-        self.name = a
+    def __init__(self, name, url):
+        self.name = unicode(slugify(name, separator=' '))
         self.url = url
         # http://readms.com/r/akame_ga_kill/065/3075/1
         self.cdn_id = url.split('/')[-2]  # get cdc id from url
