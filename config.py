75e41093f59a2fbb1c71ba91e123e453fa119e89
Rassilion <denizgezgin365@gmail.com>
2016-02-03 03:02:04 +0200

    Set autoflush false
    need for threading

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from sqlalchemy_wrapper import SQLAlchemy
 import os
 
-db = SQLAlchemy('sqlite:///manga.db')
+db = SQLAlchemy('sqlite:///manga.db', autoflush=False)
 base_url = 'http://mangastream.com/manga'
 basedir = os.path.abspath(os.path.dirname(__file__))
 outdir = os.path.join(basedir, "out")
