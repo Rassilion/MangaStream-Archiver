fa36ed3e61ba5a29145934b18dd725379535916e
Rassilion <denizgezgin365@gmail.com>
2016-07-29 13:37:36 +0300

    Fix typo

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ base_url = 'http://mangastream.com/manga'
 basedir = os.path.abspath(os.path.dirname(__file__))
 outdir = os.path.join(basedir, "out")
 
+# if true msa overwrite chapters, else mark them downloaded without downloading
 # this usefull when database is lost, helps to create new database without downloading old chapters but chapter page count still lost
 overwriteChapters = False
